8df3dc1a2886eb83148ba9f4bb20c1b721990fe5
iikallen <150830071+iikallen@users.noreply.github.com>
2026-08-16 03:50:43 +0500

    feat: add packaging permissions

--- src/UnitAtlas.Application/Tenancy/Permissions.cs
+++ src/UnitAtlas.Application/Tenancy/Permissions.cs
@@ -8,17 +8,19 @@ public static class Permissions
     public const string UnitsCreate = "units.create";
     public const string EventsRecord = "events.record";
     public const string ProductsManage = "products.manage";
+    public const string PackagingRead = "packaging.read";
+    public const string PackagingManage = "packaging.manage";
     public const string TenantManage = "tenant.manage";
 
-    public static readonly string[] All = [UnitsRead, UnitsCreate, EventsRecord, ProductsManage, TenantManage];
+    public static readonly string[] All = [UnitsRead, UnitsCreate, EventsRecord, ProductsManage, PackagingRead, PackagingManage, TenantManage];
 
     public static IReadOnlyCollection<string> For(TenantRole role) => role switch
     {
         TenantRole.Owner or TenantRole.Admin => All,
-        TenantRole.ProductionManager => [UnitsRead, UnitsCreate, EventsRecord, ProductsManage],
-        TenantRole.ProductionOperator => [UnitsRead, UnitsCreate, EventsRecord],
-        TenantRole.QualityManager or TenantRole.QualityOperator => [UnitsRead, EventsRecord],
-        TenantRole.WarehouseManager or TenantRole.WarehouseOperator => [UnitsRead, EventsRecord],
-        _ => [UnitsRead]
+        TenantRole.ProductionManager => [UnitsRead, UnitsCreate, EventsRecord, ProductsManage, PackagingRead, PackagingManage],
+        TenantRole.ProductionOperator => [UnitsRead, UnitsCreate, EventsRecord, PackagingRead, PackagingManage],
+        TenantRole.QualityManager or TenantRole.QualityOperator => [UnitsRead, EventsRecord, PackagingRead],
+        TenantRole.WarehouseManager or TenantRole.WarehouseOperator => [UnitsRead, EventsRecord, PackagingRead, PackagingManage],
+        _ => [UnitsRead, PackagingRead]
     };
 }
